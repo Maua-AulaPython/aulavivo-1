3aa9d6603c716f684e96380b5bf2c82b1b9016a3
Pedroftoledo <ppfbtoledo@gmail.com>
2015-03-20 11:45:22 -0300

    Create Ex4_string1.py

---
+++ Ex4_string1.py
@@ -0,0 +1,7 @@
+x='true'
+while x<>"false":
+	x= raw_input('Digite uma String:')
+	if x<>'false':
+		print (x[::-1])
+		print 'deseja inverter outra? se nao digite: false'
+print 'Obrigado pela preferencia'
