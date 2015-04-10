e1a0ad8fc55041a85580eae2636ead5b0adf1116
Pedroftoledo <ppfbtoledo@gmail.com>
2015-04-10 11:13:19 -0300

    Create new file

---
+++ Ex3_medias.py
@@ -0,0 +1,22 @@
+x=0
+i=0
+soma=0
+h=1
+notas=[]
+print 'Faca uma media para n numeros'
+while (x!=-1):
+	while True:
+		try:
+			x=int(raw_input('digite o %i numero: ' % h))
+			break
+		except:
+			print "voce nao digitou um numero valido"
+	if x<>-1:
+		soma= x+soma
+		i=i+1
+		h=h+1
+if i<>0:
+	resultado= soma/i
+	print 'a media e: %i' % resultado
+else:
+	print 'voce nao inseriu nenhuma nota'
